86b6a74acd7d41d56f76097c6158f0d013014efc
jimijimi <jimijimi@users.noreply.github.com>
2015-08-09 00:03:54 -0400

    Included additional cases from PLG chapter 4

--- makesdl.bat
+++ makesdl.bat
@@ -1,17 +1,28 @@
 :: windows bat file that tries to replicate the makefile behavior
 :: 2015 (c) J.Ortiz -- See copying file for license information.
 
-:: usage:  make <case>
+:: usage: make [case] <enter>
+:: to see the list of cases, type makesdl <enter>
 
 @echo off
 
 :: COMPILER OPTIONS
+:: Change the paths according to your computer setup
+:: you may need to recompile the libraries:
+::    SDL2.lib
+::    SDL2main.lib
+::    SDL2_image
+::    SDL2.dll
+::    SDL2main.dll
+::    SDL2_image.dll
 
 set CFLAGS=/MD
 set CINCW=/I"c:\\SDL2\\src\\SDL2-2.0.3\\include"
-set CLIBW=/link /LIBPATH:"C:\\SDL2\\src\\SDL2-2.0.3\\lib\\x64" SDL2.lib SDL2main.lib
+set CLIBW=/link /LIBPATH:"C:\\SDL2\\src\\SDL2-2.0.3\\lib\\x64" SDL2.lib SDL2main.lib SDL2_image.lib
 set CO=/subsystem:console
 set CDEFS=/DWINDOWS
+
+
 
 :: SDL EXAMPLES
 
@@ -27,10 +38,48 @@ if "%1" == "pixeldrawing" (
    goto COMPILERCOMMAND
 )
 
+if "%1" == "blittingsurf" (
+   set SRC=blitting_surfaces_sdl.c
+   set OUT=c4_3_blitsurf.exe
+   goto COMPILERCOMMAND
+)
+
+if "%1" == "colorkey" (
+   set SRC=colorkeys_sdl.c
+   set OUT=c4_4_colorkeys.exe
+   goto COMPILERCOMMAND
+)
+
+if "%1" == "alpha" (
+   set SRC=alpha_sdl.c
+   set OUT=c4_5_alpha.exe
+   goto COMPILERCOMMAND
+)
+
+if "%1" == "anim1" (
+   set SRC=anim1.c
+   set OUT=c4_6_anim1.exe
+   goto COMPILERCOMMAND
+)
+
+echo targets:
+echo      initsdl
+echo      pixeldrawing
+echo      blittingsurf
+echo      colorkey
+echo      alpha
+echo      anim1
+
+
 goto EXIT
 
 :COMPILERCOMMAND
 
-cl %CFLAGS% %CDEFS% %CINCW% %SRC% /Fe%OUT% %CLIBW% %CO%
+cl %CFLAGS% %CDEFS% %CINCW% %SRC% /Fe%OUT% %CLIBW% %CO% %CIMG%
+
+md objs
+md exes
+move *obj objs\
+move *exe exes\
 
 :EXIT
